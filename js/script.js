85637e39a2e66912f609897192863407bfa9311f
Nikita Kovalev <nir.vc@mail.ru>
2017-05-23 20:08:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function add () {
+	var MyTask = document.getElementById('MyTask');
+	var div1 = document.createElement('div');
+	var inputH = document.getElementById('inputH').value;
+	var inputC = document.getElementById('inputC').value;
+
+	div1.className = "taska";
+
+	var head = document.createElement('div');
+	head.className = "h";
+	head.append(inputH);
+	div1.append(head);
+
+
+	var cont = document.createElement('div');
+	cont.className = "cont";
+	cont.append(inputC);
+	div1.append(cont);
+
+	if (inputH === "" || inputC === "" ) {
+		console.log('error');
+	}else{
+		MyTask.append(div1);
+	}
+}	
+		
